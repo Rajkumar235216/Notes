408ffd6f3ef56badd62654429d570dd84e544670
Rajkumar235216 <rk235216.rk@gmail.com>
2019-07-23 10:16:25 +0530

    displayed a list of notes with eidt button.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,3 +8,12 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app';
 }
+
+//  created a constant array note[] and initialized some notes name to it
+export const NOTE :note[]=[
+  { id : 1 , name : 'Eat Breakfast'},
+  { id : 2 , name : 'Go for a Walk'},
+  { id : 3 , name : 'Learn Angular'},
+  { id : 4 , name : 'Eat Dinner'},
+  { id : 5 , name : 'Sleep'}
+];
--- src/app/notes/notes.component.ts
+++ src/app/notes/notes.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import {NOTE} from '../app.component'
 @Component({
   selector: 'app-notes',
   templateUrl: './notes.component.html',
   styleUrls: ['./notes.component.css']
 })
 export class NotesComponent implements OnInit {
-
+// imported class NOTE from app.component and initialize it in here.
   constructor() { }
+  notes = NOTE;
+
 
   ngOnInit() {
   }
